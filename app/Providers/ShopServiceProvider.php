bf10f639864eee81ed1abd8c510b813118573315
Sergey Ustsilka <ya.sg-one@yandex.ru>
2019-01-19 13:28:52 +0300

    123

--- app/Providers/ShopServiceProvider.php
+++ app/Providers/ShopServiceProvider.php
@@ -31,7 +31,7 @@ class ShopServiceProvider extends ServiceProvider
 
     public function mainSiteData(){
         View::composer(['partials.side_menu','partials.bottom_category_menu','layouts.app'], function ($views){
-            $cart = session('cart');
+            $cart = session('cart',[]);
             if (session('cart')) {
                 $cart_products = Product::query()->whereIn('id', array_keys($cart))->get();
             } else {
